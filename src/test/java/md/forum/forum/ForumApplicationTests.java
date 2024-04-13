d98ede34426ee279666cafcee3b207ebcc7ac1f5
LunguIon <lion.24072004@gmail.com>
2024-04-13 22:36:55 +0300

    first commit

---
+++ src/test/java/md/forum/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package md.forum.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
