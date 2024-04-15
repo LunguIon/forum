08587c57135bda6d6a47801548b6246cbfa43aa4
Liebe <melnicalex256@gmail.com>
2024-04-15 14:26:55 +0300

    Merge main with test

--- src/main/java/md/forum/forum/repositorys/RoleRepository.java
+++ src/main/java/md/forum/forum/repositorys/RoleRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
 public interface RoleRepository extends JpaRepository<Role,Long> {
     Optional<Role> findByRoleName(String name);
 }
