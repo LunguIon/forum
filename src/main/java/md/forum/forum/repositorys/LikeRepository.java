3578b5063e6570da2e4c148ab2c85d2f6f2f741e
Liebe <melnicalex256@gmail.com>
2024-04-19 15:37:41 +0300

    Add 'Likes' logic, endpoints and logging

--- src/main/java/md/forum/forum/repositorys/LikeRepository.java
+++ src/main/java/md/forum/forum/repositorys/LikeRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface LikeRepository {
@@ -23,6 +22,4 @@ public interface LikeRepository {
     List<Like> findAllForComments();
     @Query("SELECT l FROM likes l where l.comments != null and l.comments = :coment")
     List<Like> findAllForComments(Comment comment);
-    @Query("SELECT l FROM likes l where l.comments != null and l.comments = :coment and l.post = :post")
-    List<Like> findAllForComments(Comment comment, Post post);
 }
