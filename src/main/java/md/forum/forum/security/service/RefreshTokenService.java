d77326e42d936d2d3898e64a5611ea17beb971ca
LunguIon <lion.24072004@gmail.com>
2024-04-29 14:01:35 +0300

    added the logs folder in gitignore also did some small refactoring

--- src/main/java/md/forum/forum/security/service/RefreshTokenService.java
+++ src/main/java/md/forum/forum/security/service/RefreshTokenService.java
@@ -22,8 +22,8 @@ public class RefreshTokenService {
         this.userRepository = userRepository;
     }
 
-    public RefreshToken createRefreshToken(String username) {
-        Optional<User> userOptional = userRepository.findByEmail(username);
+    public RefreshToken createRefreshToken(String email) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             RefreshToken refreshToken = RefreshToken.builder()
@@ -33,14 +33,14 @@ public class RefreshTokenService {
                     .build();
             return refreshTokenRepository.save(refreshToken);
         } else {
-            throw new IllegalArgumentException("User not found for username : " + username);
+            throw new IllegalArgumentException("User not found for username : " + email);
         }
 
     }
 
 
-    public Optional<RefreshToken> findByToken(String username) {
-        return refreshTokenRepository.findByToken(username);
+    public Optional<RefreshToken> findByToken(String email) {
+        return refreshTokenRepository.findByToken(email);
     }
 
     public RefreshToken verifyExpiration(RefreshToken refreshToken) {
