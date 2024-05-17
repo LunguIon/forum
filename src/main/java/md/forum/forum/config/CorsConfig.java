4c72737f223b53cc22b215bd713c4e331eca0e48
Liebe <melnicalex256@gmail.com>
2024-05-17 19:10:36 +0300

    Fix: Another try: CORS Problem

---
+++ src/main/java/md/forum/forum/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package md.forum.forum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://speakapi.lol");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
