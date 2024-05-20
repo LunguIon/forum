1563592995abc329be09e05ab0fbde0e24b946e9
Liebe <melnicalex256@gmail.com>
2024-05-20 11:26:00 +0300

    Another try to fix CORS, please work

--- src/main/java/md/forum/forum/security/config/SecurityConfiguration.java
+++ src/main/java/md/forum/forum/security/config/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://speakapi.lol/login"));
+        configuration.setAllowedOrigins(List.of("https://speakapi.lol"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type","Origin","Accept"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
