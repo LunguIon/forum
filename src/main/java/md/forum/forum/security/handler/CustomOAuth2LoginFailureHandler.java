9458a46585ed469d0863305e91b395ff7b530c09
LunguIon <lion.24072004@gmail.com>
2024-05-21 12:58:15 +0300

    Feat:Added custom error handler for oauth2

---
+++ src/main/java/md/forum/forum/security/handler/CustomOAuth2LoginFailureHandler.java
@@ -0,0 +1,19 @@
+package md.forum.forum.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomOAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        setDefaultFailureUrl("http://localhost:4200/error");
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
