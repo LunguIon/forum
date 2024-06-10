712de085c5645687aa75dde6bae3e2ec1c8c7ce4
Liebe001 <melnicalex256@gmail.com>
2024-06-10 14:34:31 +0300

    Set up for git

--- src/main/java/md/forum/forum/config/SwaggerConfig.java
+++ src/main/java/md/forum/forum/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .servers(
                         List.of(
-                                new Server().url("https://speakapi.lol")
+                                new Server().url("http://localhost:8080")
                         )
                 )
                 .info(
